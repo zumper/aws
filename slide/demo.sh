6567fd29bfd6c99e78fc831b42995681a2d615e4
Stuart Tettemer <stu@zumper.com>
2014-04-26 13:01:34 -0700

    Build the client-main, not gen-main, call ec2-query

--- slide/demo.sh
+++ slide/demo.sh
@@ -3,6 +3,6 @@ tree gen
 ./gen-main/gen-main ./2014-02-01-ec2.wsdl > gen/20140201/ec2/ec2.go
 tree gen
 head gen/20140201/ec2/ec2.go
-go build -o ec2-query gen-main/main.go
+go build -o ec2-query client-main/main.go
 sleep 15
-./client-main/client-main $(head -1 ~/.aws-gophercon) $(tail -1 ~/.aws-gophercon) us-west-1 i-ddbebd82 i-dcbebd83 | less
+./ec2-query $(head -1 ~/.aws-gophercon) $(tail -1 ~/.aws-gophercon) us-west-1 i-ddbebd82 i-dcbebd83 | less
